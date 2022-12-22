d50dd14831b2dafebb3b87f38e2752677230c077
Mink <minhndgdh190046@fpt.edu.vn>
2022-12-22 14:20:55 +0700

    fix after 20/12/2022

---
+++ ConcreteRooms/Bedroom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.ConcreteRooms
+{
+    public class Bedroom : Room
+    {
+        public Bedroom()
+        {
+            Color = "";
+            Style = "";
+            Area = 0.00;
+        }
+        public Bedroom(string color, string style, double area)
+        {
+            Color = color;
+            Style = style;
+            Area = area;
+        }
+        public override string Visualize()
+        {
+            return "Bedroom with Color: " + Color + ", Style: " + Style + ", Area: " + Area + "m2";
+        }
+        public override double TotalCost()
+        {
+            return 24.00 * Area;
+        }
+        public override string Suggestion()
+        {
+            return "SYSTEM SUGGESTION - Bedroom: 2 Doors, 1 Table, 2 Chairs, 0 Bathtub, 1 Fan\n" +
+                "Do you want to use this suggestion? (y/n)";
+        }
+    }
+}
